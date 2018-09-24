170d5525dc3a96a10d18ce9cccbafbbf2cab0a0a
samkratzer <43553291+samkratzer@users.noreply.github.com>
2018-09-24 16:50:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+var numOne = document.getElementById("num-one");
+var numTwo = document.getElementById("num-two");
+var addSum = document.getElementById("add-sum");
+var img = document.getElementById("the-rock");
 
+numOne.addEventListener("input", add);
+numTwo.addEventListener("input", add);
+
+function add() {
+
+var one = parseFloat(numOne.value) || 0;
+var two = parseFloat(numTwo.value) || 0;
+
+img.className = "hide"
+addSum.innerHTML = display(one+two);
+}
+
+function display(number){
+	if(number>10000){
+		img.className = "animated-gif";
+		return "wow, that's a lot!";
+	}
+	else if (number<10){
+		return "not very much";
+	}
+	else return "your number is: " + number;
+}
