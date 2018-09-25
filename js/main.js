aa674791453e04d95c11c4c405a570b88988ec32
samkratzer <43553291+samkratzer@users.noreply.github.com>
2018-09-25 12:59:32 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,14 +5,18 @@ var rock = document.getElementById("the-rock");
 var toad = document.getElementById("toad");
 
 numOne.addEventListener("input", add);
-numOne.addEventListener("input", trumpcheck);
 numTwo.addEventListener("input", add);
-numTwo.addEventListener("input", trumpcheck);
 
 function add() {
 
 var one = parseFloat(numOne.value) || 0;
 var two = parseFloat(numTwo.value) || 0;
+var string = (numOne.value + numTwo.value).toLowerCase;
+
+	if(string.contains("trump"){
+	   toad.className = "animated-gif";
+	   }
+	else toad.className = "hide";
 
 rock.className = "hide"
 addSum.innerHTML = display(one+two);
@@ -27,18 +31,5 @@ function display(number){
 		return "not very much";
 	}
 	else return "your number is: " + number;
-}
-
-function trumpcheck(){
-
-	var stringOne = numOne.value.toLowerCase();
-	var stringTwo = numTwo.value.toLowerCase();
-	var stringCheck = stringOne.concat(stringTwo);
-	
-	if(stringCheck.contains("trump"){
-	   rock.className = "hide";
-	   toad.className = "animated-gif";
-	   }
-	else toad.className = "hide";
 }
 	   
