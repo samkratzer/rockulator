15edf520e690cd62b7350608fc04a8581820b733
samkratzer <43553291+samkratzer@users.noreply.github.com>
2018-09-25 14:03:44 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,24 +12,23 @@ function add() {
 
 var one = parseFloat(numOne.value) || 0;
 var two = parseFloat(numTwo.value) || 0;
-var passedString = (numOne.value + numTwo.value) || 0;
-var myString = passedString.toLowerCase();
+var myString = (numOne.value + numTwo.value).toLowerCase;
 
 toad.className = "hide";
 rock.className = "hide";
-addSum.innerHTML = display(one+two, myString);
+addSum.innerHTML = display(one+two);
 stringDisplay.innnerHTML = trumpcheck(myString);
 }
 
 function display(number){
-	if(number>10000) {
-		rock.className = "animated-gif";
-		return "hey... that's a pretty big number";
-	}
-	else if (number<10) {
-		return "not very much";
-	}
-	else return "your number is: " + number;
+	if(number>10000){
+	rock.className = "animated-gif";
+ 		return "hey... that's a pretty big number";
+ 	}
+ 	else if (number<10){
+ 		return "not very much";
+ 	}
+ 	else return "your number is: " + number;
 }
 	  
 function trumpcheck(text){
@@ -37,5 +36,8 @@ function trumpcheck(text){
 		toad.className = "big-splash";
 		return "yeti pubes and a dick like the mushroom character from mario kart...";
 	}
-	else return null;
-}
+ 	else{
+ 		toad.className = "hide";
+ 		return text;
+ 	}
+ }
